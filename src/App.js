f2b22a0116e26136ffd86c3ce27e58847bfdf0fc
Neil Brown <neilbrown08@hotmail.com>
2020-11-19 16:56:16 +0000

    added color selector to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStore } from 'effector-react';
 import cn from 'classnames';
 
 import { connect, send } from './network';
 import { gameService } from './services/game';
 import { Player } from './components/Player';
+import { ColorSelector } from './components/ColorSelector';
 
 import globalStyles from './styles/global.module.scss';
 import './styles/styles.scss';
 
 function App() {
   const [name, setName] = useState('');
+  const [showColorSelector, setShowColorSelector] = useState(false);
   const { room } = useStore(gameService.$);
+
+  useEffect(() => {
+    gameService.colorRequested.watch(_ => {
+      setShowColorSelector(true);
+    });
+  }, []);
 
   const onChange = (event) => {
     setName(event.target.value);
@@ -23,6 +31,11 @@ function App() {
     } catch (error) {
       console.error(`Error connecting`, error);
     }
+  };
+
+  const handleColorSelector = (colorId) => {
+    setShowColorSelector(false);
+    send('colorSelected', colorId);
   };
 
   return (
@@ -40,6 +53,7 @@ function App() {
       {room?.stack &&
         room.stack.map((card, index) => <div key={card.id} style={{ transform: `translate(-50%, -50%) rotate(${index * 10}deg)` }} className={cn(globalStyles.card, globalStyles[card.className], globalStyles.played)}></div>)}
       </section>
+      {showColorSelector && <ColorSelector clickHandler={handleColorSelector} />}
       {room?.players &&
         room.players.map((player) => (
           <Player key={player.name} send={send} isRunning={room?.isRunning} onMessage={room?.onMessage} player={player} />
--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import {gameService} from '../../services/game';
+import React from 'react';
 import cn from 'classnames';
-
-import { ColorSelector } from '../ColorSelector';
 
 import globalStyles from '../../styles/global.module.scss';
 import styles from './styles.module.scss';
 
 export const Player = ({ isRunning, send, player }) => {
-  const [showColorSelector, setShowColorSelector] = useState(false);
-
-  useEffect(() => {
-    gameService.colorRequested.watch(_ => {
-      setShowColorSelector(true);
-    });
-  }, []);
-
   const playCard = (id) => {
     send('playCard', id);
   };
@@ -24,23 +13,19 @@ export const Player = ({ isRunning, send, player }) => {
     send('start');
   };
 
-  const handleColorSelector = (colorId) => {
-    setShowColorSelector(false);
-    send('colorSelected', colorId);
-  };
-
   return (
     <div>
       <p>
         {player.name}:{player.id}
         {!isRunning && !player.isOwner && ' Waiting...'}
       </p>
+
       {!isRunning && player.id && player.isOwner && (
         <button type="button" onClick={startGame}>
           Start Game
         </button>
       )}
-      {showColorSelector && <ColorSelector clickHandler={handleColorSelector} />}
+
       <ul className={styles.cards}>
         {player.cards
           ? player.cards.map((card) => (
